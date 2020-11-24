fb327bfc76056e5d4351bdfc013558687c0fc3b8
ravismd <56954294+ravismd@users.noreply.github.com>
2020-11-24 13:23:35 +0530

    Update filterLogs.groovy

--- vars/filterLogs.groovy
+++ vars/filterLogs.groovy
@@ -6,7 +6,7 @@ def call(String filter_string, int occurrence) {
     def logs = currentBuild.rawBuild.getLog(10000).join('\n')
     int count = StringUtils.countMatches(logs, filter_string);
     if (count > occurrence -1) {
-        currentBuild.description = 'Build is success but manually tweeted sorry for this'
+        currentBuild.description = "Build is success but manually tweeted sorry for this"
         currentBuild.result='UNSTABLE'
     }
 }
